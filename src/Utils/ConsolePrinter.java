36939b4a9f37cb9f24c48a4b07123d0df57eacfe
Soulaiman Bouhlal <bouhlal.soulaiman@gmail.com>
2024-09-20 12:55:13 +0100

    modif: update the Menu Principale

--- src/Utils/ConsolePrinter.java
+++ src/Utils/ConsolePrinter.java
@@ -6,24 +6,31 @@ import Entities.Projet;
 import Utils.Types.CostBreakdown;
 
 public class ConsolePrinter {
+
     public static void mainMenu(){
         System.out.println(" +----------------------------------------+");
-        System.out.println(" |               Main Menu                |");
+        System.out.println(" |           professionnel Menu           |");
         System.out.println(" +----------------------------------------+");
         System.out.println(" |  1. Create a new project               |");
-        System.out.println(" |  2. Add a Client                       |");
-//        System.out.println(" |  2. Show existing projects             |");
-//        System.out.println(" |  3. Calculate the cost of a project    |");
-        System.out.println(" |  4. Exit                               |");
+        System.out.println(" |  2. Add Components to a Project        |");
+        System.out.println(" |  3. Get Details of a Project           |");
+        System.out.println(" +----------------------------------------+");
+        System.out.println(" |               Client Menu              |");
+        System.out.println(" +----------------------------------------+");
+        System.out.println(" |  4. Add a Client                       |");
+        System.out.println(" |  5. Accept/Refuse Devis                |");
+        System.out.println(" +----------------------------------------+");
+        System.out.println(" |  6. Exit                               |");
         System.out.println(" +----------------------------------------+");
         System.out.print(" ==> Enter your choice: ");
     }
+
     public static void clientMenu(){
         System.out.println(" +----------------------------------------+");
         System.out.println(" |               Client Menu              |");
         System.out.println(" +----------------------------------------+");
-        System.out.println(" |  1. Search for an existing customer    |");
-        System.out.println(" |  2. Add a new customer                 |");
+        System.out.println(" |  1. Search for an existing Client    |");
+        System.out.println(" |  2. Add a new Client                 |");
         System.out.println(" |  4. Exit                               |");
         System.out.println(" +----------------------------------------+");
         System.out.print(" ==> Enter your choice: ");
@@ -33,15 +40,15 @@ public class ConsolePrinter {
         String redText = "\033[0;31m";
         String resetText = "\033[0m";
 
-        System.out.println("\n" + redText + "--------------------------------------");
+        System.out.println(redText + "--------------------------------------");
         System.out.println( message );
-        System.out.println("--------------------------------------" + resetText + "\n" );
+        System.out.println("--------------------------------------" + resetText);
     }
 
     public static void printClient(Client client){
         String redText = "\033[0;31m";
         String resetText = "\033[0m";
-        System.out.println("\n" + redText);
+        System.out.println(redText);
         System.out.println(" +----------------------------------------+");
         System.out.println(" |                 Client                 |");
         System.out.println(" +----------------------------------------+");
@@ -50,14 +57,15 @@ public class ConsolePrinter {
         System.out.println(" | PhoneNumber   | "  + client.getPhoneNumber());
         System.out.println(" | Professional  | "  + client.getProfessional());
         System.out.println(" +----------------------------------------+");
-        System.out.println(resetText + "\n" );
+        System.out.println(resetText);
 
     }
 
     public static void printCostDetails(CostBreakdown costBreakdown){
         String redText = "\033[0;33m";
         String resetText = "\033[0m";
-        System.out.println("\n" + redText);
+
+        System.out.println(redText);
         System.out.println(" +----------------------------------------+");
         System.out.println(" |           Project's Cost [MAD]          |");
         System.out.println(" +----------------------------------------+");
@@ -67,32 +75,37 @@ public class ConsolePrinter {
         System.out.println(" +----------------------------------------+");
         System.out.println(" | Total Cost      | "  + costBreakdown.getTotalCost());
         System.out.println(" +----------------------------------------+");
-        System.out.println(resetText + "\n" );
+        System.out.println(resetText);
 
     }
 
     public static void  printDevis(Devis devis){
         String redText = "\033[0;33m";
         String resetText = "\033[0m";
-        System.out.println("\n" + redText);
+        System.out.println( redText);
         System.out.println(" +----------------------------------------+");
         System.out.println(" |                  Devis                 |");
         System.out.println(" +----------------------------------------+");
-        System.out.println(" | Client's Name     | " + devis.getProjet().getClient().getName());
-        System.out.println(" | Project's Name    | " + devis.getProjet().getProjectName());
-        System.out.println(" | Project's Cost    | " + devis.getProjet().getTotalCost());
-        System.out.println(" | Devis is Accepted | "  + devis.getAccepted());
+        System.out.println(" | Devis's ID        | " + devis.getId());
+        if (devis.getProjet().getClient() != null){
+            System.out.println(" | Client's Name     | " + devis.getProjet().getClient().getName());
+        }
+        if (devis.getProjet().getId() != null){
+            System.out.println(" | Project's ID      | " + devis.getProjet().getId());
+            System.out.println(" | Project's Name    | " + devis.getProjet().getProjectName());
+        }
+        System.out.println(" | Project's Cost    | " + devis.getEstimatedPrice());
+        System.out.println(" | Devis is Accepted | "  + (devis.getAccepted() ? "Accepted" : "Rejected"));
         System.out.println(" | Valid Until       | "  + devis.getValidityDate().toString());
         System.out.println(" +----------------------------------------+");
-        System.out.println(resetText + "\n" );
+        System.out.println(resetText);
     }
 
     public static void printSuccess(String message){
         String greenText = "\033[0;32m";
         String resetText = "\033[0m";
-        System.out.println("\n" + greenText + "--------------------------------------");
+        System.out.println(greenText + "--------------------------------------");
         System.out.println(" " +  message );
-        System.out.println("--------------------------------------" + resetText + "\n" );
-
+        System.out.println("--------------------------------------" + resetText  );
     }
 }
